07687ec5835afd98dacd35dcd520aaa283465c23
KomalKahlon <162742713+KomalKahlon@users.noreply.github.com>
2024-04-11 15:16:56 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,32 +1,22 @@
 
 """
-Menu Module
-
-This module provides the main menu interface for the Employee Management System.
-Users can interact with the application through the menu options, which include
-adding, deleting, updating employee information, generating reports, and exiting
-the application.
-"""
 from .employee_operations import add_employee, delete_employee, update_employee
 from .report_generation import generate_reports
+
+def main_menu():
 def main_meenu()
     """
-    
-    Main Menu Function
-    Displays the main menu for the Employee Management System and prompts the user for input.
-    Based on the user's choice, it calls the corresponding functions to perform actions such as
-    adding, deleting, or updating employee information, generating reports, or exiting the program.
-    """
-    while True:
+
+
+
+while True:
         print("\nEmployee Management System")
         print("1. Add Employee")
         print("2. Delete Employee")
         print("3. Update Employee")
         print("4. Generate Reports")
         print("5. Exit Exit")
-
         choice = input("Enter your choice: ")
-
         if choice == "1":
             add_employee()
         elif choice == "2":
