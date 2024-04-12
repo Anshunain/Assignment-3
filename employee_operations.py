83ddf15a5b65d3c7b617c758acfce3e6daa1bd4a
Anshu nain <162193609+Anshunain@users.noreply.github.com>
2024-04-11 20:50:45 -0400

    Update employee_operations.py

--- employee_operations.py
+++ employee_operations.py
@@ -15,7 +15,7 @@ ID = int(input("please enter the ID for the new employee: "))
             
     """
     Add Employee Function
-    if employee_name = add:
+    def add(employee detail)
     return ("first_Name, last_name, date_of_birth, position_hold, salary")
 
     # This function prompts the user to input details for a new employee and adds the employee
@@ -26,11 +26,16 @@ ID = int(input("please enter the ID for the new employee: "))
 def delete_employee():
 ID = int(input("ID for delete the employees: ")
          First_name = Input("first name of the employees: ")
-         last_name =   Input("last name of the employees: ")      
-         
+         last_name =   Input("last name of the employees: ")  
+         date_of_birth = input("date of birth of employees in (YYYY/MM/DD) ")
+         start_of_year = int(input("employee's starting year "))
+         position_hold = input("employee's hold position")
+         salary = float(input("employee's salary "))
     
     """
     Delete Employee Function
+    def delete(employee details)
+    return none
 
     This function prompts the user to input the ID of the employee to be deleted and
     removes the employee from the system.
@@ -41,18 +46,12 @@ def update_employee():
     ID = int(input("ID of employee to be updated "))
     for employee in employees:
         if employee.id == id:
-            first_name = input("new first name of the employee ")
+            first_name = input("name of the employee ")
             last_name = input("new last name of the employee ")
             date_of_birth = input("new date_of_birth of the employ (YYYY/MM/DD) ")
             start_of_year = int(input(new start year of employee ")
             position_update = input("enter new position updated of the employees ")
             salary = float(input("new salary of the employee ")
-                           
-            employee.first_name = first_name
-            employee.last_name = last_name
-            employee.date_of_birth = date_of_birth
-            employee.salary = salary
-            employee.position = position
 
             print(f"employee with ID {ID}update ")
             return
